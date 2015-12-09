6fb8a7f23936c5c19b10692ecb11be81e3f267e6
Marcel Hecko <maco@blava.net>
2015-12-09 14:51:12 +0100

    initial version of the python scripts to get the data from serial interface and push them into local litesql database

---
+++ lib/tools.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+import sqlite3
+import logging
+import __main__
+import subprocess
+import ConfigParser
+
+def insert_to_db(blob):
+    db_file_path = '/tmp/env_monitoring.db'
+    conn = sqlite3.connect(db_file_path, timeout=5)
+    c = conn.cursor()
+    try:
+      new_data = (int(time.time()), 'lora', gw_serial(), blob, 0)
+      if __main__.args.verbose:
+          print new_data
+      c.execute("INSERT INTO data (timestamp, source, gateway_serial, payload, state) VALUES (?,?,?,?,?)", (new_data))
+      conn.commit()
+      conn.close()
+    except Exception as e:
+      conn.close()
+      raise
+    return True
+
+def gw_serial():
+    serial = os.popen('cat /proc/cpuinfo | grep Serial | awk \'{ print $NF }\'').read().rstrip()
+    if not serial:
+        serial = 'unknown'
+    return serial
